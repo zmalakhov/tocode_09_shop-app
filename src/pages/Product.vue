8685ca5b2e0dd0e08e48e96fa8203730a9e00776
zmalakhov <zm.13.15.zm@gmail.com>
2020-06-14 14:42:03 +0300

    #4 - Динамика - Динамические роуты и параметры роута

---
+++ src/pages/Product.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+        <section>
+            <div class="container">
+                <h1>Product: {{id}}</h1>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                id: this.$route.params.id
+            }
+        },
+        created() {
+            console.log(this.$route);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ Vue.use(Router)
 import Home from '@/pages/Home'
 import NotFound from '@/pages/404'
 import Shop from '@/pages/Shop'
+import Product from '@/pages/Product'
 
 // Routering
 export default new Router({
@@ -22,6 +23,11 @@ export default new Router({
             path: '/shop',
             name: 'shop',
             component: Shop
+        },
+        {
+            path: '/shop/:id',
+            name: 'product',
+            component: Product
         },
         {
             path: '*',
