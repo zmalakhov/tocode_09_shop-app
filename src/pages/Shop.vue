5d5c5149585e3308f3732657f8b38261d8545904
zmalakhov <zm.13.15.zm@gmail.com>
2020-06-14 14:07:45 +0300

    #3 - Работа с стейтом - Проходимся по массиву из стейта через отдельный компонент

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -3,14 +3,26 @@
         <section>
             <div class="container">
                 <h1>Shop page</h1>
+                <div class="item__wrapper">
+                    <shopItem
+                            v-for="product in shopList" :key="product.id"
+                            :product="product"
+                    >
+
+                    </shopItem>
+                </div>
             </div>
         </section>
     </div>
 </template>
 
 <script>
+
+    import shopItem from '@/components/ShopItem.vue'
+
     export default {
-        data(){
+        components: {shopItem},
+        data() {
             return {
                 shopList: null
             }
--- src/store/shop.js
+++ src/store/shop.js
@@ -6,6 +6,24 @@ export default {
                 title: 'Nike Red',
                 descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
                 img: require('../assets/img/1.png')
+            },
+            {
+                id: 2,
+                title: 'Nike Default',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+                img: require('../assets/img/4.png')
+            },
+            {
+                id: 3,
+                title: 'Nike Green',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+                img: require('../assets/img/7.png')
+            },
+            {
+                id: 4,
+                title: 'Nike Street',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+                img: require('../assets/img/10.png')
             }
         ]
     },
